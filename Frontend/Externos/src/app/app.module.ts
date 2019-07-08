faac69152e61deb33fa9ec07972e5ee82d2e3b56
Alejandro <edarevalo01@gmail.com>
2019-07-08 15:23:06 -0500

    First Commit
    -Estructura básica de carpetas
    -Creación de proyecto Angular
    -Integración de PrimeNG

---
+++ Frontend/Externos/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+
+import { ButtonModule } from "primeng/button";
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    ButtonModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
